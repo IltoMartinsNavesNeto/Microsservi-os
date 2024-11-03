bfb7c9715efb46b74def5aec4aa6e96ff299c20f
IltoMartinsNavesNeto <iltoneto400@gmail.com>
2024-11-03 17:35:11 -0300

    Feign client

--- hr-payroll/src/main/java/com/iltoneto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/iltoneto/hrpayroll/services/PaymentService.java
@@ -10,21 +10,16 @@ import org.springframework.web.client.RestTemplate;
 
 import com.iltoneto.hrpayroll.entities.Payment;
 import com.iltoneto.hrpayroll.entities.Worker;
+import com.iltoneto.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
-
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
